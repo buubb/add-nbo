d8a345c5ca21b467606fb4b18e5a1648052426b7
buubb <qls773322@naver.com>
2024-03-11 10:30:39 +0900

    [yubin] add-nbo.cpp Update

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -23,12 +23,19 @@ uint32_t add_nbo(char **argv){
 	uint32_t res;
 	
 	fp1 = fopen(argv[1],"rb");
-        fread(num1, 1, 4, fp1);
+	fp2 = fopen(argv[2],"rb");
+
+        if (fp1 == NULL or fp2 == NULL){
+		printf("File open failed..\n");
+		return 1;
+	}
+
+	fread(num1, 1, 4, fp1);
 	int fileLength1 = ftell(fp1);
 
-        fp2 = fopen(argv[2],"rb");
         fread(num2, 1, 4, fp2);
 	int fileLength2 = ftell(fp2);
+	
 
 	uint32_t* p1 = reinterpret_cast<uint32_t*>(num1);
 	uint32_t* p2 = reinterpret_cast<uint32_t*>(num2);
@@ -39,7 +46,8 @@ uint32_t add_nbo(char **argv){
 	
 	if (fileLength1 < 4 or fileLength2 < 4){
 		printf("File size is small than 4bytes.\n");
-	} else{
+	}	
+	else{
 		printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",a,a,b,b,res,res);
 	}
 
